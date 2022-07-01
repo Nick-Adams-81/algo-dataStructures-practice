5c7ac4e1abc24e09ea6072a8a517a1a5b85bc756
Nick Adams <nick.pappas.adams@gmail.com>
2022-07-01 09:35:14 -0500

    added valid BST

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,7 +1,27 @@
-# print("hello")
-def say_hello():
-    print("hello world")
 
+# ------- Binary Tree ------- #
 
+# invert binary tree
+def invertTree(self, root):
+    if root is None:
+        return None
 
-say_hello()
+    tmp = root.left
+    root.left = root.right
+    root.right = tmp
+
+    self.invertTree(root.left)
+    self.invertTree(root.left)
+    return root
+
+# is valid BST
+def isValidBST(root):
+    def valid(node, left, right):
+        if not node:
+            return True
+
+        if not (node.val < right and node.val > left):
+            return False
+
+        return(valid(node.left, left, node.val) and valid(node.right, node.val, right))
+    return valid(root, float("-inf"), float("inf"))
